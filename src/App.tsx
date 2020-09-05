651a7c0625d3b95ab4980e6984ba570f856c790d
igaue-takahhiko <hasewo.2610@gmail.com>
2020-09-06 04:47:47 +0900

    implement selectBox SwitchCountry component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Cards from './features/covid/Cards/Cards';
 import Chart from './features/covid/Chart/Chart';
 import PieChart from './features/covid/PieChart/PieChart';
+import SwitchCountry from './features/covid/SwitchCountry/SwitchCountry';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Cards />
             <Chart />
             <PieChart />
+            <SwitchCountry />
         </header>
         </div>
     );
