c45547b5a28a475e0603c691bb855ea93e857909
igaue-takahhiko <hasewo.2610@gmail.com>
2020-09-05 10:48:07 +0900

    implement sliceFile

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import covidReducer from '../features/covid/covidSlice'
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+        covid: covidReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
